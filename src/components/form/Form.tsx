2851f0b655e196044daa6b245370a9fd6ddfa12e
Your Name <you@example.com>
2024-11-29 12:27:05 +0500

    clean the code

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -5,24 +5,109 @@ import ArrowLeft from '../../icons/ArrowLeft';
 import CheckMark from '../../icons/CheckMark';
 import Eye from '../../icons/Eye';
 import NoEye from '../../icons/NoEye';
+import { InputFieldType, TextareaFieldType } from '../../types';
 
 const Form = () => {
   const [description, setDescription] = useState('');
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const [isVisible, setIsVisible] = useState(true);
 
-  const handleButtonClick = (visible: boolean) => {
-    setIsVisible(visible);
+  const handleButtonClick = (visible: boolean) => setIsVisible(visible);
+
+  const handleFileInputClick = () => fileInputRef.current?.click();
+
+  const handleChangeDescription = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    e.preventDefault();
+    setDescription(e.target.value);
   };
 
-  const handleFileInputClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
+  const InputField = ({
+    label,
+    id,
+    type,
+    placeholder,
+    value,
+    handleChange,
+  }: InputFieldType) =>
+    type === 'text' ? (
+      <div>
+        <label
+          htmlFor={id}
+          className="block text-sm font-semibold text-gray-700"
+        >
+          {label}
+        </label>
+        <input
+          type={type}
+          id={id}
+          value={value}
+          onChange={handleChange}
+          placeholder={placeholder}
+          className="border border-gray-200 rounded-md p-3 mt-2 w-full focus:outline-none bg-blue-100 opacity-50 placeholder-gray-700"
+        />
+      </div>
+    ) : type === 'checkbox' ? (
+      <div className="flex items-center">
+        <input
+          type="checkbox"
+          id="isMainClub"
+          value={value}
+          onChange={handleChange}
+          className="mr-2 h-5 w-5 border-gray-300 text-blue-500 focus:ring-0 bg-blue-100 opacity-[0.5] "
+        />
+        <label htmlFor={id} className="text-sm text-gray-700">
+          {label}
+        </label>
+      </div>
+    ) : null;
+
+  const WordCount = ({ text }: { text: string }) => {
+    const countWords = (text: string) =>
+      text.trim().split(/\s+/).filter(Boolean).length;
+    const wordCount = countWords(text);
+    return (
+      <p
+        className={`text-xs mx-5 ${
+          wordCount > 300 ? 'text-red-500' : 'text-gray-500'
+        }`}
+      >
+        {wordCount > 300
+          ? 'Максимальное количество слов превышено!'
+          : `${wordCount}/300`}
+      </p>
+    );
   };
 
-  const countWords = (text: string) => {
-    return text.trim().split(/\s+/).filter(Boolean).length;
+  const UploadFile = () => {
+    return (
+      <>
+        <label htmlFor="fileInput" className="block text-gray-500 font-[600]">
+          Логотип клуба
+        </label>
+
+        <div className="">
+          <div
+            className="flex gap-1 items-center justify-center py-10 px-4 rounded-md border border-dashed border-blue-600  cursor-pointer font-bold text-blue-600 text-lg"
+            onClick={handleFileInputClick}
+          >
+            <UploadDoc />
+            Загрузить файл
+          </div>
+          <input
+            ref={fileInputRef}
+            type="file"
+            id="fileInput"
+            className="hidden"
+          />
+          <p className="text-gray-400 leading-snug font-[500] text-[12px]">
+            JPG, PNG, SVG <br />
+            Не более 600x600px, 5MB
+          </p>
+        </div>
+      </>
+    );
   };
 
   return (
@@ -34,56 +119,26 @@ const Form = () => {
 
       <div className="flex gap-7">
         <div className="basis-[40%] flex flex-col gap-2">
-          <label htmlFor="fileInput" className="block text-gray-500 font-[600]">
-            Логотип клуба
-          </label>
-
-          <div className="">
-            <div
-              className="flex gap-1 items-center justify-center py-10 px-4 rounded-md border border-dashed border-blue-600  cursor-pointer font-bold text-blue-600 text-lg"
-              onClick={handleFileInputClick}
-            >
-              <UploadDoc />
-              Загрузить файл
-            </div>
-            <input
-              ref={fileInputRef}
-              type="file"
-              id="fileInput"
-              className="hidden"
-            />
-            <p className="text-gray-400 leading-snug font-[500] text-[12px]">
-              JPG, PNG, SVG <br />
-              Не более 600x600px, 5MB
-            </p>
-          </div>
+          <UploadFile />
         </div>
 
         <div className="basis-[60%] flex flex-col gap-7">
           <div>
-            <label
-              htmlFor="clubName"
-              className="block text-sm font-semibold text-gray-700"
-            >
-              Название клуба
-            </label>
-            <input
+            <InputField
               type="text"
+              label="Название клуба"
               id="clubName"
-              className="border border-gray-200 rounded-md p-3 mt-2 w-full focus:outline-none bg-blue-100 opacity-[0.5] placeholder-gray-700"
               placeholder="Введите название клуба"
             />
           </div>
 
           <div className="flex items-center">
-            <input
-              type="checkbox"
+            <InputField
+              placeholder=""
+              label="Главный клуб"
               id="isMainClub"
-              className="mr-2 h-5 w-5 border-gray-300 text-blue-500 focus:ring-0 bg-blue-100 opacity-[0.5] "
+              type="checkbox"
             />
-            <label htmlFor="isMainClub" className="text-sm text-gray-700">
-              Главный клуб
-            </label>
           </div>
 
           <div>
@@ -119,32 +174,19 @@ const Form = () => {
           id="description"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          className="border border-gray-200 rounded-md p-3 mt-2 w-full focus:outline-none bg-blue-100 opacity-[0.5] placeholder-gray-700 h-[100px]"
+          className="border border-gray-200 rounded-md p-3 mt-2 w-full focus:outline-none bg-blue-100 opacity-50 placeholder-gray-700 h-[100px]"
           placeholder="Текст описания"
         />
-        <p
-          className={`text-xs mx-5 ${
-            countWords(description) > 300 ? 'text-red-500' : 'text-gray-500'
-          }`}
-        >
-          {countWords(description) > 300
-            ? 'Максимальное количество слов превышено!'
-            : countWords(description) + '/300'}
-        </p>
+        <WordCount text={description} />
       </div>
 
       <div className="flex gap-5">
         <div className="basis-1/2">
-          <label
-            htmlFor="category"
-            className="block text-sm font-semibold text-gray-700"
-          >
-            Категория
-          </label>
-          <input
-            type="text"
+          <InputField
+            label="Категория"
             id="category"
-            className="border border-gray-200 rounded-md p-3 mt-2 w-full focus:outline-none bg-blue-100 opacity-[0.5] placeholder-gray-700"
+            placeholder=""
+            type="text"
           />
         </div>
 
@@ -160,7 +202,7 @@ const Form = () => {
             className="border border-blue-200 rounded-md p-1 mt-2 w-full focus:outline-none bg-blue-50 placeholder-gray-700 h-[49.5px] flex relative "
           >
             <button
-              className={`rounded-lg absolute p-2 top-1 left-1 w-[44%] h-fit flex gap-1 justify-center items-center transition-all duration-500 ease-in-out ${
+              className={`rounded-lg absolute p-2 top-1 left-1 gap-1 w-[44%] h-fit flex justify-center items-center transition-all duration-500 ease-in-out ${
                 isVisible
                   ? 'bg-blue-500 text-white'
                   : 'text-gray-600 bg-transparent'
@@ -172,7 +214,7 @@ const Form = () => {
             </button>
 
             <button
-              className={`absolute top-1 right-1 w-[54%] h-fit p-2 flex gap-1 justify-center items-center transition-all duration-500 ease-in-out ${
+              className={`absolute top-1 right-1 w-[54%] gap-1 h-fit p-2 flex justify-center items-center transition-all duration-500 ease-in-out ${
                 !isVisible
                   ? 'bg-blue-500 text-white rounded-lg'
                   : 'bg-transparent text-gray-600'
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,7 @@ export type StudentInfoType = {
   name: string;
   surname: string;
   img: string;
-}
+};
 
 export type TableItemType = {
   id: number;
@@ -10,11 +10,27 @@ export type TableItemType = {
   created_at: string;
   status: string;
   student_info: StudentInfoType;
-}
+};
 
 export type TableEntryType = {
   id: number;
   title: string;
   status: 'pending' | 'declined' | 'approved';
   table_items: TableItemType[];
-}
+};
+
+export type InputFieldType = {
+  label: string;
+  id: string;
+  type?: string;
+  placeholder: string;
+  value?: string;
+  handleChange?: (e: any) => void;
+};
+
+export type TextareaFieldType = {
+  label: string;
+  id: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+};
